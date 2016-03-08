abac990edacc3914627a30cf631aae7e857f3b51
sbksba <sbksba@gmail.com>
2016-03-08 17:59:07 +0100

    [ADD] Ajout des scripts principaux

---
+++ launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+###############
+# UTILISATION #
+###############
+# ./MediaBot `cat config.txt`   => no verbose mode
+# ./MediaBot v `cat config.txt` => verbose mode
+#./MediaBot.pl v `cat config/config.txt`
+./MediaBot.pl v `cat config/config_test.txt`
