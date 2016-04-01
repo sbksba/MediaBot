2fcdcaac93b4d343964d99d439945b024946a651
sbksba <sbksba@gmail.com>
2016-04-01 14:39:15 +0200

    [DEL] Mise au propre

--- launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-###############
-# UTILISATION #
-###############
-# ./MediaBot `cat config.txt`   => no verbose mode
-# ./MediaBot v `cat config.txt` => verbose mode
-#./MediaBot.pl v `cat config/config.txt`
-./MediaBot.pl v `cat config/config_test.txt`
