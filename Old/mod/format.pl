0aac1f9321ea3d77fc4512ad90c89c252e639ea9
sbksba <sbksba@gmail.com>
2017-12-12 15:44:13 +0100

    [DEL] delete the old version in Perl

--- Old/mod/format.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl -s
-require 'mod/MediaLIB.pm';
-$dir ||= ".";
-$mode ||= 0;
-MediaLIB::my_format($dir,$mode);
--- Old/test/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#===========================#
-# Programme de test : erase #
-#===========================#
-
-rm -f *.avi *.mp4 *.mkv *~
-rm -rf action animation comedie documentaire drame espionnage fantastique science_fiction spectacle super_heros thriller xtrem none The* Y* G* P* C* V* W* B* M* N* E* H* L* R* S* 12* D* F* V*
